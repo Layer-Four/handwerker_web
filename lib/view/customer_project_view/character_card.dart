0eddf4e97e1616782e7f68e8fd499b686cf38808
obaidajarrah <153510718+obaidajarrah@users.noreply.github.com>
2024-03-26 13:37:50 +0100

    projectübersicht

--- lib/view/customer_project_view/character_card.dart
+++ lib/view/customer_project_view/character_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:handwerker_web/view/customer_project_view/custom_project.dart';
 
+// ignore: must_be_immutable
 class CharacterCard extends StatefulWidget {
   final CustomeProject project;
   final bool isLast;
@@ -26,8 +27,8 @@ class _CharacterCardState extends State<CharacterCard> {
       decoration: BoxDecoration(
         border: Border(
           top: const BorderSide(),
-          left: const BorderSide(),
-          right: const BorderSide(),
+          // left: const BorderSide(),
+          // right: const BorderSide(),
           bottom: widget.isLast ? const BorderSide() : BorderSide.none,
         ),
       ),
@@ -113,166 +114,94 @@ class _CharacterCardState extends State<CharacterCard> {
               ),
             ),
             if (widget.isContainerOpen)
-              Container(
-                padding: const EdgeInsets.all(8),
-                margin: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Colors.grey),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Row(
+                children: <Widget>[
+                  const Flexible(
+                    flex: 4, // 20%
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Berlin Ag',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text(
-                              'KundenNummer:134D8282',
-                              style: TextStyle(color: Colors.grey),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: screenWihdthinProzents * 11),
-                        Expanded(
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: Text(
-                              '2',
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                        Text('11 h'),
                         Text(
-                          '1.099 EUR ',
-                          overflow: TextOverflow.ellipsis,
+                          "Berlin AG",
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          '100.00 EUr EUR ',
-                          overflow: TextOverflow.ellipsis,
+                          'Kundennummer:134D8282',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text('Kontakt'),
+                        Text(
+                          'Ansprechpartner:Jürgen Holz\nTelefon:016037372\nE-Mail:kunden@handwerker.de',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text('Adresse:'),
+                        Text(
+                          'Berliner Straße 234\n2290 Berlin',
+                          style: TextStyle(color: Colors.grey),
                         ),
                       ],
                     ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                  Flexible(
+                    flex: 2, // 60%
+                    child: Container(
+                      height: 160,
+                      decoration: BoxDecoration(border: Border.all()),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('Der Kunde\nist blöd'),
+                          Text('Antwortet nie'),
+                          Text('Zahlt nicht'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 3, // 20%
+                    child: Column(
                       children: [
-                        Expanded(
-                          flex: 2,
-                          child: Column(
+                        const Center(child: Text("2")),
+                        const SizedBox(
+                          height: 22,
+                        ),
+                        Container(
+                          height: 100,
+                          width: 200,
+                          decoration: BoxDecoration(border: Border.all()),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                'Kontakt:',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Text(
-                                'Ansprechpartner:Jürgen Holz',
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              Text(
-                                'Telefon:016037372',
-                                style: TextStyle(color: Colors.grey),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Text(
-                                'E-mail:Kunde@handwerker.de',
-                                style: TextStyle(color: Colors.grey),
-                              ),
+                              Text("1 Begründung Herkules"),
+                              Text("2 Begründung Schlosswiese"),
                             ],
                           ),
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            height: 180,
-                            width: 120,
-                            decoration: BoxDecoration(border: Border.all()),
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 12, horizontal: 1),
-                                child: Text(
-                                  'Der Kunde\nist blöd \nantwortet nie\nzahlt nicht',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Text(
-                              '1 Begründung Herkules \n 2 Begründung Schlosswiese'),
-                        ),
-                        Column(
-                          children: [
-                            Text('5 h '),
-                            Text('6 h '),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('1000 EUR  '),
-                            Text('99 EUR '),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('10.000 EUR '),
-                            Text('990.000 EUR '),
-                          ],
-                        ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Kontakt:',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text(
-                              'Adresse:',
-                              style: TextStyle(color: Colors.grey),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text(
-                              'Berliner Straße 234',
-                              style: TextStyle(color: Colors.grey),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text(
-                              '22990 Berlin',
-                              style: TextStyle(color: Colors.grey),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+                  ),
+                  Flexible(
+                    flex: 1, // 60%
+                    child: Container(color: Colors.green),
+                  ),
+                  Flexible(
+                    flex: 1, // 60%
+                    child: Container(
+                        color: const Color.fromARGB(255, 175, 76, 134)),
+                  ),
+                  Flexible(
+                    flex: 1, // 60%
+                    child: Container(color: Colors.green),
+                  ),
+                ],
+              )
           ],
         ),
       ),
