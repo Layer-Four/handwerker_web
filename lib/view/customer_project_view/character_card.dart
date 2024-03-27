cb59a689b1a30b7ff635e66754cf004ac1b70941
obaidajarrah <153510718+obaidajarrah@users.noreply.github.com>
2024-03-27 13:38:09 +0100

    kundenübersicht new Design Implement

--- lib/view/customer_project_view/character_card.dart
+++ lib/view/customer_project_view/character_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:handwerker_web/view/customer_project_view/custom_project.dart';
 
 // ignore: must_be_immutable
@@ -35,6 +37,7 @@ class _CharacterCardState extends State<CharacterCard> {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             GestureDetector(
               onTap: () {
@@ -45,79 +48,76 @@ class _CharacterCardState extends State<CharacterCard> {
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    SizedBox(
-                      width: screenWihdthinProzents * 25,
-                      child: Row(
-                        children: [
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              widget.project.customer,
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 20,
-                      child: Text(
-                        widget.project.description,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 11,
-                      child: Text(
-                        '${widget.project.projectNumber}',
-                        textAlign: TextAlign.right,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 11,
-                      child: Text(
-                        '${widget.project.projectNumber} EUR',
-                        textAlign: TextAlign.right,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 11,
-                      child: Text(
-                        '${widget.project.totalTime} EUR',
-                        textAlign: TextAlign.right,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 11,
-                      child: Text(
-                        '${widget.project.costItems} EUR',
-                        textAlign: TextAlign.right,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWihdthinProzents * 11,
-                      child: Text(
-                        '${widget.project.revenue} EUR',
-                        textAlign: TextAlign.right,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Icon(widget.isContainerOpen
-                        ? Icons.arrow_drop_up
-                        : Icons.arrow_drop_down)
+                    // SizedBox(
+                    //   width: screenWihdthinProzents * 20,
+                    //   child: Text(
+                    //     widget.project.description,
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   width: screenWihdthinProzents * 11,
+                    //   child: Text(
+                    //     '${widget.project.projectNumber}',
+                    //     textAlign: TextAlign.right,
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   width: screenWihdthinProzents * 11,
+                    //   child: Text(
+                    //     '${widget.project.projectNumber} EUR',
+                    //     textAlign: TextAlign.right,
+                    //     overflow: TextOverflow.ellipsis,
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   width: screenWihdthinProzents * 11,
+                    //   child: Text(
+                    //     '${widget.project.totalTime} EUR',
+                    //     textAlign: TextAlign.right,
+                    //     overflow: TextOverflow.ellipsis,
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   width: screenWihdthinProzents * 11,
+                    //   child: Text(
+                    //     '${widget.project.costItems} EUR',
+                    //     textAlign: TextAlign.right,
+                    //     overflow: TextOverflow.ellipsis,
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   width: 260,
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.project.customer,
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          '${widget.project.revenue} EUR',
+                          textAlign: TextAlign.right,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Icon(widget.isContainerOpen
+                            ? Icons.arrow_drop_up
+                            : Icons.arrow_drop_down),
+                      ],
+                    )
                   ],
                 ),
               ),
             ),
+            const SizedBox(
+              height: 25,
+            ),
             if (widget.isContainerOpen)
-              Row(
-                children: <Widget>[
-                  const Flexible(
-                    flex: 4, // 20%
+              const Row(
+                children: [
+                  Flexible(
+                    flex: 5, // 20%
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -148,58 +148,213 @@ class _CharacterCardState extends State<CharacterCard> {
                       ],
                     ),
                   ),
-                  Flexible(
-                    flex: 2, // 60%
-                    child: Container(
-                      height: 160,
-                      decoration: BoxDecoration(border: Border.all()),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text('Der Kunde\nist blöd'),
-                          Text('Antwortet nie'),
-                          Text('Zahlt nicht'),
-                        ],
-                      ),
-                    ),
+                  SizedBox(
+                    width: 16,
                   ),
+
                   Flexible(
-                    flex: 3, // 20%
+                    flex: 18,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        const Center(child: Text("2")),
-                        const SizedBox(
-                          height: 22,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Proeject 1'),
+                            Text('20.000 \$'),
+                          ],
                         ),
-                        Container(
-                          height: 100,
-                          width: 200,
-                          decoration: BoxDecoration(border: Border.all()),
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("1 Begründung Herkules"),
-                              Text("2 Begründung Schlosswiese"),
-                            ],
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Proeject 2'),
+                            Text('20.000 \$'),
+                          ],
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Proeject 2'),
+                            Text('20.000 \$'),
+                          ],
+                        )
                       ],
                     ),
-                  ),
-                  Flexible(
-                    flex: 1, // 60%
-                    child: Container(color: Colors.green),
-                  ),
-                  Flexible(
-                    flex: 1, // 60%
-                    child: Container(
-                        color: const Color.fromARGB(255, 175, 76, 134)),
-                  ),
-                  Flexible(
-                    flex: 1, // 60%
-                    child: Container(color: Colors.green),
-                  ),
+                  )
+                  // Flexible(
+                  //   flex: 22,
+                  //   child:
+                  //       // Column(
+                  //       //   children: [
+                  //       //     const Center(child: Text("2")),
+                  //       //     const SizedBox(
+                  //       //       height: 22,
+                  //       //     ),
+                  //       //     Container(
+                  //       //       height: 100,
+                  //       //       width: 200,
+                  //       //       decoration: BoxDecoration(border: Border.all()),
+                  //       //       child: const Column(
+                  //       //         mainAxisAlignment: MainAxisAlignment.center,
+                  //       //         crossAxisAlignment: CrossAxisAlignment.start,
+                  //       //         children: [
+                  //       //           Text("1 Begründung Herkules"),
+                  //       //           Text("2 Begründung Schlosswiese"),
+                  //       //         ],
+                  //       //       ),
+                  //       //     ),
+                  //       //   ],
+                  //       // ),
+
+                  //       Table(
+                  //     border: const TableBorder(
+                  //       bottom:
+                  //           BorderSide(), // Ensure bottom border for the table
+                  //       right:
+                  //           BorderSide(), // Ensure bottom border for the table
+                  //     ),
+                  //     defaultVerticalAlignment:
+                  //         TableCellVerticalAlignment.middle,
+                  //     columnWidths: const {
+                  //       0: FlexColumnWidth(
+                  //           2), // Setting the width of the first column to be 2 times the width of other columns
+                  //     },
+                  //     children: [
+                  //       TableRow(
+                  //         decoration: const BoxDecoration(
+                  //           border: Border(
+                  //             bottom: BorderSide(),
+                  //             // top: BorderSide()
+                  //           ), // Border for the header row
+                  //         ),
+                  //         children: [
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                   right:
+                  //                       BorderSide(), // Only add right border for Header 1
+                  //                   // top: BorderSide
+                  //                   //     .none, // Remove top border for Header 1
+                  //                   // left: BorderSide
+                  //                   //     .none, // Remove left border for Header 1
+                  //                 ),
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('2'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                   top:
+                  //                       BorderSide(), // Only add right border for Header 1
+                  //                 ),
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('1 h'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                   top:
+                  //                       BorderSide(), // Only add right border for Header 1
+                  //                 ),
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('1.099 EUR'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                   top:
+                  //                       BorderSide(), // Only add right border for Header 1
+                  //                 ),
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('100.00 EUR'),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //       TableRow(
+                  //         decoration: const BoxDecoration(
+                  //             border: Border(left: BorderSide())),
+                  //         children: [
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                     right:
+                  //                         BorderSide()), // Add right border for this cell
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('1 Begrünung Herkules'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('5 h'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('1000 EUR'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('10.000 EUR'),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //       TableRow(
+                  //         decoration: const BoxDecoration(
+                  //             border: Border(left: BorderSide())),
+                  //         children: [
+                  //           TableCell(
+                  //             child: Container(
+                  //               decoration: const BoxDecoration(
+                  //                 border: Border(
+                  //                     right:
+                  //                         BorderSide()), // Add right border for this cell
+                  //               ),
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('2 Begrünung Herkules'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('6 h'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('99 EUR'),
+                  //             ),
+                  //           ),
+                  //           TableCell(
+                  //             child: Container(
+                  //               padding: const EdgeInsets.all(8.0),
+                  //               child: const Text('990.000 EUR'),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                 ],
               )
           ],
