8ba98f5b4e81aef3095f4f906aa8b1419c607f61
LuigiRetzkowski <luigi.retzkowski@web.de>
2024-04-05 14:30:01 +0200

    Finishing frontend projectoverview

--- lib/view/customer_project_view/doc_screen.dart
+++ lib/view/customer_project_view/doc_screen.dart
@@ -8,7 +8,7 @@ void docScreen() {
 
 class MyApp extends StatelessWidget {
   final List<CustomeProject> project = [
-    const CustomeProject(
+    CustomeProject(
       'Layer Four GmbH',
       'Austausch der\nHeizungsanlage',
       false,
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       1009,
       1009,
     ),
-    const CustomeProject(
+    CustomeProject(
       'Berlin AG',
       'Tisch gebaut',
       true,
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       2099,
       9000,
     ),
-    const CustomeProject(
+    CustomeProject(
       'Creative GmbH',
       'Fenster eingesetzt',
       true,
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       2,
       900000,
     ),
-    const CustomeProject(
+    CustomeProject(
       'Fio Bestmann',
       'Steinloch 43\n22880, Hamburg',
       true,
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
       2,
       1009,
     ),
-    const CustomeProject(
+    CustomeProject(
       'Florian hensel',
       'große Straße 54\n22449, Kassel',
       true,
@@ -66,101 +66,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(75, 30, 30, 30),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  const Text('Berichte',
-                      style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold)),
-                  Container(
-                    width: 800,
-                    child: const Padding(
-                      padding: EdgeInsets.only(
-                        left: 80,
-                      ),
-                      child: Material(
-                        elevation: 5,
-                        //  borderRadius: BorderRadius.circular(16),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            isDense: true,
-                            border: OutlineInputBorder(
-                              //      borderRadius: BorderRadius.circular(16),
-                              borderSide: BorderSide(width: 1.0, color: Colors.grey),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blueGrey),
-                            ),
-                            errorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.redAccent),
-                            ),
-                            focusedErrorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.orangeAccent),
-                            ),
-                            disabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            contentPadding: EdgeInsets.all(8.0),
-                            hintText: 'Suche...',
-                            suffixIcon: Icon(
-                              Icons.search,
-                              color: Colors.grey,
-                            ),
-                            suffixStyle: TextStyle(color: Colors.green),
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(75, 30, 30, 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Text('Berichte',
+                    style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold)),
+                Container(
+                  width: 800,
+                  child: const Padding(
+                    padding: EdgeInsets.only(
+                      left: 80,
+                    ),
+                    child: Material(
+                      elevation: 5,
+                      //  borderRadius: BorderRadius.circular(16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(width: 1.0, color: Colors.grey),
                           ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueGrey),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.redAccent),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orangeAccent),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          contentPadding: EdgeInsets.all(8.0),
+                          hintText: 'Suche...',
+                          suffixIcon: Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                          ),
+                          suffixStyle: TextStyle(color: Colors.green),
                         ),
                       ),
                     ),
                   ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Kunde',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  // Text('Beschreibung', style: TextStyle(color: Colors.grey)),
+                  // Text('Anzahl Projekte', style: TextStyle(color: Colors.grey)),
+                  // Text('Gesamtzeit', style: TextStyle(color: Colors.grey)),
+                  // Text('Materialkosten', style: TextStyle(color: Colors.grey)),
+                  Text('Umsatz', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    width: 140,
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'Kunde',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Spacer(),
-                    // Text('Beschreibung', style: TextStyle(color: Colors.grey)),
-                    // Text('Anzahl Projekte', style: TextStyle(color: Colors.grey)),
-                    // Text('Gesamtzeit', style: TextStyle(color: Colors.grey)),
-                    // Text('Materialkosten', style: TextStyle(color: Colors.grey)),
-                    Text('Umsatz', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                    SizedBox(
-                      width: 140,
-                    ),
-                  ],
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            SizedBox(
+              width: screenWidth > 600 ? double.infinity : null,
+              height: MediaQuery.of(context).size.height - 300,
+              child: ListView.builder(
+                itemCount: project.length,
+                itemBuilder: (_, index) => CharacterCard(
+                  project[index],
+                  isFirst: index == 0,
+                  isLast: index == project.length,
                 ),
               ),
-              const SizedBox(
-                height: 60,
-              ),
-              SizedBox(
-                width: screenWidth > 600 ? double.infinity : null,
-                height: MediaQuery.of(context).size.height - 300,
-                child: ListView.builder(
-                  itemCount: project.length,
-                  itemBuilder: (_, index) => CharacterCard(
-                    project[index],
-                    isFirst: index == 0,
-                    isLast: index == project.length,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/view/customer_project_view/project_card.dart
+++ lib/view/customer_project_view/project_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:handwerker_web/view/customer_project_view/custom_project.dart';
+import 'package:handwerker_web/view/customer_project_view/project_details.dart';
 import 'package:handwerker_web/view/customer_project_view/project_overview.dart';
 
 // ignore: must_be_immutable
@@ -9,14 +10,12 @@ class ProjectCard extends StatefulWidget {
   final CustomeProject project;
   final bool isFirst;
   final bool isLast;
-  bool isContainerOpen;
 
   ProjectCard(
     this.project, {
     super.key,
     this.isFirst = false,
     this.isLast = false,
-    this.isContainerOpen = false,
   });
 
   @override
@@ -24,90 +23,111 @@ class ProjectCard extends StatefulWidget {
 }
 
 class _ProjectCardState extends State<ProjectCard> {
+  bool isContainerOpen = false;
+
   @override
   Widget build(BuildContext context) {
     final screenWidthInPercent = (MediaQuery.of(context).size.width / 100);
-
-    return Row(
+    return Column(
       children: [
-        SizedBox(width: screenWidthInPercent * 2),
-        Container(
-          // width: double.infinity,
-          width: screenWidthInPercent * 70,
-          decoration: BoxDecoration(
-            border: Border(
-              top: widget.isFirst ? BorderSide.none : const BorderSide(),
-              // left: const BorderSide(),
-              // right: const BorderSide(),
-              bottom: widget.isLast ? const BorderSide() : BorderSide.none,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    widget.project.customer,
-                    //    textAlign: TextAlign.right,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
+        Row(
+          children: [
+            SizedBox(width: screenWidthInPercent * 2),
+            Container(
+              // width: double.infinity,
+              width: screenWidthInPercent * 70,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: widget.isFirst ? BorderSide.none : const BorderSide(),
+                  // left: const BorderSide(),
+                  // right: const BorderSide(),
+                  bottom: widget.isLast ? const BorderSide() : BorderSide.none,
                 ),
-                //  const Spacer(),
-                Expanded(
-                  flex: 2,
-                  child: widget.project.status
-                      ? const Text(
-                          'Abgeschlossen',
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isContainerOpen = !isContainerOpen;
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 8,
+                        child: Text(
+                          widget.project.customer,
+                          //    textAlign: TextAlign.right,
+                          style: const TextStyle(
                             fontSize: 16,
-                            color: Colors.green,
-                          ),
-                        )
-                      : const Text(
-                          'Offen',
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.orange,
                           ),
                         ),
-                ),
-                //   const Spacer(),
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    widget.project.projectTimeWindow,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                //  const Spacer(),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Text(
-                      '${widget.project.revenue},- €',
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(
-                        fontSize: 16,
                       ),
-                    ),
+                      //  const Spacer(),
+                      Expanded(
+                        flex: 8,
+                        child: widget.project.status
+                            ? const Text(
+                                'Abgeschlossen',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.green,
+                                ),
+                              )
+                            : const Text(
+                                'Offen',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                      ),
+                      //   const Spacer(),
+                      Expanded(
+                        flex: 8,
+                        child: Text(
+                          widget.project.projectTimeWindow,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      //  const Spacer(),
+                      Expanded(
+                        flex: 8,
+                        child: Container(
+                          child: Text(
+                            '${widget.project.revenue},- €',
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.right,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Icon(isContainerOpen ? Icons.arrow_drop_up : Icons.arrow_drop_down),
+                      )
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
+        isContainerOpen
+            ? ProjectDetails(
+                widget.project,
+              )
+            : Container(),
       ],
     );
   }
