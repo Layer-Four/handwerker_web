5aeb51bdb46e9c2da46d5aec6c2aae2ff1574ebf
MmeissnerLayer4 <153509730+MmeissnerLayer4@users.noreply.github.com>
2024-06-21 14:03:51 +0200

    remove validation on responsedata thats doesend used

--- lib/provider/data_provider/time_entry_provider/time_entry_provider.dart
+++ lib/provider/data_provider/time_entry_provider/time_entry_provider.dart
@@ -70,12 +70,9 @@ class TimeVMNotifier extends Notifier<List<TimeVMAdapter>> {
           'Error on saveTimeEntry, status-> ${response.statusCode}\n ${response.data}',
         );
       }
-      final jsonResponse = response.data;
-      final data = jsonResponse.map((e) => TimeVMAdapter.fromJson(e)).toList();
-      if (state.isNotEmpty) {
-        final newstate = <TimeVMAdapter>[...state, data];
-        state = newstate;
-      }
+      // final jsonResponse = response.data;
+      // jsonResponse.map((e) => TimeVMAdapter.fromJson(e)).toList();
+
       return true;
     } on DioException catch (e) {
       throw Exception('DioException: ${e.message}');
