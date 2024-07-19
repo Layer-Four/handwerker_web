f144c7f23e910e92f0103b153dd714c682f49693
MmeissnerLayer4 <153509730+MmeissnerLayer4@users.noreply.github.com>
2024-07-19 09:28:17 +0200

    init and change AppColor colors all Colors-> Color.fromARGB

--- lib/constants/themes/app_color.dart
+++ lib/constants/themes/app_color.dart
@@ -1,42 +1,40 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color kPrimaryButtonColor = const Color.fromARGB(255, 224, 142, 60);
-  static Color kPrimary = const Color.fromARGB(255, 98, 98, 98);
-  static Color kWhite = Colors.white;
-  static Color kGreyBackground = const Color.fromRGBO(238, 238, 238, 1);
-  static Color kBlack = Colors.black;
-  static Color kTextfieldBorder = const Color.fromARGB(255, 220, 217, 217);
-  static Color kTextfieldColor = const Color.fromARGB(211, 245, 241, 241);
-  static Color kWhiteWOpacity = const Color.fromARGB(192, 255, 255, 255);
-  static Color kBlue = Colors.blue;
-  static Color kRed = Colors.red;
-  static Color kGrey = Colors.grey;
-  static Color kGreen = Colors.green;
-  static Color kYellow = Colors.yellow;
-  Color get white => Colors.white;
-  Color get black => Colors.black;
-  static const appbarGreen = Color.fromARGB(255, 76, 141, 95);
+  static const Color kPrimaryButtonColor = Color.fromARGB(255, 224, 142, 60);
+  static const Color kPrimary = Color.fromARGB(255, 98, 98, 98);
+  static const Color kWhite = Color.fromARGB(255, 255, 255, 255);
+  static const Color kGreyBackground = Color.fromRGBO(238, 238, 238, 1);
+  static const Color kTextfieldBorder = Color.fromARGB(255, 220, 217, 217);
+  static const Color kTextfieldColor = Color.fromARGB(211, 245, 241, 241);
+  static const Color kWhiteWOpacity = Color.fromARGB(192, 255, 255, 255);
+  static const Color kBlack = Color.fromARGB(255, 0, 0, 0);
+  static const Color kBlue = Color.fromARGB(255, 33, 150, 243);
+  static const Color kRed = Color.fromARGB(255, 244, 67, 54);
+  static const Color kGrey = Color.fromARGB(255, 158, 158, 158);
+  static const Color kGreen = Color.fromARGB(255, 76, 175, 80);
+  static const Color kYellow = Color.fromARGB(255, 255, 235, 59);
+  static const Color appbarGreen = Color.fromARGB(255, 76, 141, 95);
 
-  static const MaterialColor materialColorPrimary = MaterialColor(_materialprimary, <int, Color>{
+  static const MaterialColor materialColorPrimary = MaterialColor(0xFF626262, <int, Color>{
     50: Color(0xFFECECEC),
     100: Color(0xFFD0D0D0),
     200: Color(0xFFB1B1B1),
     300: Color(0xFF919191),
     400: Color(0xFF7A7A7A),
-    500: Color(_materialprimary),
+    500: Color(0xFF626262),
     600: Color(0xFF5A5A5A),
     700: Color(0xFF505050),
     800: Color(0xFF464646),
     900: Color(0xFF343434),
   });
-  static const int _materialprimary = 0xFF626262;
+  // static const int _materialprimary = 0xFF626262;
 
-  static const MaterialColor materialprimaryAccent = MaterialColor(_materialprimaryAccentValue, <int, Color>{
+  static const MaterialColor materialprimaryAccent = MaterialColor(0xFFEE6969, <int, Color>{
     100: Color(0xFFF39797),
-    200: Color(_materialprimaryAccentValue),
+    200: Color(0xFFEE6969),
     400: Color(0xFFFF2525),
     700: Color(0xFFFF0C0C),
   });
-  static const int _materialprimaryAccentValue = 0xFFEE6969;
+  // static const int _materialprimaryAccentValue = 0xFFEE6969;
 }
